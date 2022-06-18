e2cff27052d0d38f142768c231fd30b76b965556
Sasha <q.dev.null@gmail.com>
2022-06-18 07:29:28 +0200

    Add the basic tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,10 +1,8 @@
 import "mocha";
+import store from "../src/store";
 
-var assert = require('assert');
-describe('Array', function () {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal([1, 2, 3].indexOf(4), -1);
-        });
+describe('Store', function () {
+    it("dispatches the event", () => {
+        console.log(store)
     });
 });
