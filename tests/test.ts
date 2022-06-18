66d0d209faf4b6bf8d073cec18f8c4fe0ea0b9db
Sasha <q.dev.null@gmail.com>
2022-06-18 08:31:29 +0200

    Add the remove item tests

--- tests/test.ts
+++ tests/test.ts
@@ -4,9 +4,12 @@ import { addItem, removeItem } from "../src/actions";
 
 describe('Store', function () {
     it("dispatches the event", () => {
-        console.log("Before the actions")
-        console.log(store.getState())
+        console.log("Before the actions");
+        console.log(store.getState());
         store.dispatch(addItem("test item"));
-        console.log(store.getState())
+        console.log(store.getState());
+        store.dispatch(removeItem(0));
+        console.log("After remove item")
+        console.log(store.getState());
     });
 });
