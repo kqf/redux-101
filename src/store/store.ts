5eb39abff26862a6e863e16930b417e645d6fe27
kqf <q.dev.null@gmail.com>
2022-06-21 16:22:13 +0200

    Switch to redux toolkit (#11)
    * Add the reduxjs-toolkit
    
    * Use the create store function

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,8 @@
-import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./orders";
 
+const store = configureStore({
+    reducer: reducer as any,
+});
 
-const store = createStore(reducer as any);
 export default store;
