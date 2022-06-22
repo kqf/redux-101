568961452b416248decca2a023c684a9364f2a3d
Sasha <q.dev.null@gmail.com>
2022-06-22 07:47:37 +0200

    Add the tests

--- tests/test.ts
+++ tests/test.ts
@@ -4,12 +4,12 @@ import { addedItem, dispatchedItem, removedItem } from "../src/store/orders";
 
 describe('Store', function () {
     it("dispatches the event", () => {
-        store.dispatch(addedItem("test item"));
+        store.dispatch(addedItem({ description: "test item" }));
 
         console.log("Before dispatch", store.getState());
-        store.dispatch(dispatchedItem(0));
+        store.dispatch(dispatchedItem({ id: 0 }));
         console.log("After dispatch", store.getState());
 
-        store.dispatch(removedItem(0));
+        store.dispatch(removedItem({ id: 0 }));
     });
 });
