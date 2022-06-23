d232e689ac712467aacfcccae30d34e51d52ed8e
Sasha <q.dev.null@gmail.com>
2022-06-23 09:10:21 +0200

    Fix the vendors tests

--- tests/testVendors.ts
+++ tests/testVendors.ts
@@ -1,25 +1,19 @@
-import { configureStore } from "@reduxjs/toolkit";
 import "mocha";
-import { addedItem, dispatchedItem, removedItem } from "../src/store/orders";
+import store from "../src/store/store";
 
 import { createVendor, removeVendor } from "../src/store/vendors";
-import reducer from "../src/store/vendors"
 const assert = require('assert');
 
 
 describe('Store', function () {
     it("dispatches the event", () => {
-        const store = configureStore({
-            reducer: reducer as any,
-        });
-
-        assert.strictEqual(store.getState().length, 0);
+        assert.strictEqual(store.getState().entities.vendors.length, 0);
         store.dispatch(createVendor({ name: "Vendor1" }));
 
-        assert.strictEqual(store.getState().length, 1);
-        assert.strictEqual(store.getState()[0].id, 1);
+        assert.strictEqual(store.getState().entities.vendors.length, 1);
+        assert.strictEqual(store.getState().entities.vendors[0].id, 1);
 
         store.dispatch(removeVendor({ id: 1 }));
-        assert.strictEqual(store.getState().length, 0);
+        assert.strictEqual(store.getState().entities.vendors.length, 0);
     });
 });
