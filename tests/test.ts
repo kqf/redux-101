f088d183d5dfe6783fbd73b353c96551682634ea
Sasha <q.dev.null@gmail.com>
2022-06-23 09:08:19 +0200

    Fix the state in tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,21 +1,20 @@
 import "mocha";
 import store from "../src/store/store";
 import { addedItem, dispatchedItem, removedItem } from "../src/store/orders";
-import { hasExpectedRequestMetadata } from "@reduxjs/toolkit/dist/matchers";
 const assert = require('assert');
 
 describe('Store', function () {
     it("dispatches the event", () => {
-        assert.strictEqual(store.getState().length, 0);
+        assert.strictEqual(store.getState().entities.orders.length, 0);
         store.dispatch(addedItem({ description: "test item" }));
 
-        assert.strictEqual(store.getState().length, 1);
-        assert.strictEqual(store.getState()[0].dispatched, false);
+        assert.strictEqual(store.getState().entities.orders.length, 1);
+        assert.strictEqual(store.getState().entities.orders[0].dispatched, false);
 
         store.dispatch(dispatchedItem({ id: 0 }));
-        assert.strictEqual(store.getState()[0].dispatched, true);
+        assert.strictEqual(store.getState().entities.orders[0].dispatched, true);
 
         store.dispatch(removedItem({ id: 0 }));
-        assert.strictEqual(store.getState().length, 0);
+        assert.strictEqual(store.getState().entities.orders.length, 0);
     });
 });
