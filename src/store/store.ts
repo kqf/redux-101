a8b51c67a1a4534b39fa2032db474f20cca2d1c8
Sasha <q.dev.null@gmail.com>
2022-06-25 09:40:47 +0200

    Add the missing slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,14 @@
 import { combineReducers, configureStore, Store } from "@reduxjs/toolkit";
 import orderReducer from "./orders";
 import vendorReducer from "./vendors";
+import ownerReducer from "./owners";
 
 const store: Store = configureStore({
     reducer: combineReducers({
         entities: combineReducers({
             orders: orderReducer,
             vendors: vendorReducer,
+            owners: ownerReducer,
         }),
     })
 });
