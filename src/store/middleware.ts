ed42ea6cd9ac66503f13b7ff8be224f2fb0fe569
Sasha <q.dev.null@gmail.com>
2022-06-26 21:18:57 +0200

    Fix the naming

--- src/store/middleware.ts
+++ src/store/middleware.ts
@@ -1,7 +1,7 @@
 import { configureStore, Action, Middleware } from '@reduxjs/toolkit';
 import { RootState } from './store';
 
-const customMiddleware: Middleware<{}, RootState> = store => next => action => {
+const customLogger: Middleware<{}, RootState> = store => next => action => {
     console.log(store.getState());
     const res = next(action);
     console.log(res);
