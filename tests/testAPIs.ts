ff3888ad2c6c9f0cb483ef05c833d52146433f0a
Sasha <q.dev.null@gmail.com>
2022-06-27 10:13:05 +0200

    Add the test that checks for apis

---
+++ tests/testAPIs.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import nock from "nock";
+
+const fetchData = async () => {
+    const res = await axios.get('https://api.example.com');
+    const data = res.data;
+    return data;
+}
+
+describe('expectedData', () => {
+
+    it("checks if API returns expected data", async () => {
+        nock('https://api.example.com')
+            .get('/')
+            .reply(200, {
+                data: {
+                    id: 1,
+                    title: "The weather is nice",
+                    completed: true
+                }
+            });
+        const results = await fetchData();
+        expect(results.data.title).toEqual("The weather is nice");
+    });
+});
