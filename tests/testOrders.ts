7e987359522cf16fd0b4bf93068c13e8e7078769
Sasha <q.dev.null@gmail.com>
2022-06-29 20:11:36 +0200

    Fix the order tests

--- tests/testOrders.ts
+++ tests/testOrders.ts
@@ -1,32 +1,33 @@
 import "mocha";
+import { expect } from "chai";
 import { addedItem, dispatchedItem, removedItem, selectOrdersLength, assignItemOwner, selectOrdersOf } from "../src/store/orders";
 import store from "../src/store/store";
 const assert = require('assert');
 
 describe('Orders', function () {
     it("dispatches the event", () => {
-        assert.strictEqual(selectOrdersLength(store.getState()), 0);
+        expect(selectOrdersLength(store.getState())).to.equal(0);
         store.dispatch(addedItem({ description: "test item1" }));
 
-        assert.strictEqual(selectOrdersLength(store.getState()), 1);
-        assert.strictEqual(store.getState().entities.orders[0].dispatched, false);
+        expect(selectOrdersLength(store.getState())).to.equal(1);
+        expect(store.getState().entities.orders[0].dispatched).to.equal(false);
 
         store.dispatch(dispatchedItem({ id: 0 }));
-        assert.strictEqual(store.getState().entities.orders[0].dispatched, true);
-        assert.strictEqual(store.getState().entities.orders[0].ownerId, undefined);
+        expect(store.getState().entities.orders[0].dispatched).to.equal(true);
+        expect(store.getState().entities.orders[0].ownerId).to.equal(undefined);
 
-        assert.strictEqual(selectOrdersOf(0)(store.getState()).length, 0);
+        expect(selectOrdersOf(0)(store.getState()).length).to.equal(0);
         store.dispatch(assignItemOwner({ id: 0, ownerId: 0 }));
 
-        assert.strictEqual(store.getState().entities.orders[0].ownerId, 0);
+        expect(store.getState().entities.orders[0].ownerId).to.equal(0);
         store.dispatch(addedItem({ description: "test item2" }));
-        assert.strictEqual(selectOrdersOf(0)(store.getState()).length, 1);
+        expect(selectOrdersOf(0)(store.getState()).length).to.equal(1);
         store.dispatch(assignItemOwner({ id: 1, ownerId: 0 }));
-        assert.strictEqual(selectOrdersOf(0)(store.getState()).length, 2);
+        expect(selectOrdersOf(0)(store.getState()).length).to.equal(2);
 
         store.dispatch(removedItem({ id: 0 }));
-        assert.strictEqual(selectOrdersLength(store.getState()), 1);
-        assert.strictEqual(selectOrdersOf(0)(store.getState()).length, 1);
+        expect(selectOrdersLength(store.getState())).to.equal(1);
+        expect(selectOrdersOf(0)(store.getState()).length).to.equal(1);
     });
 
     it("Middleware catches an error", () => {
