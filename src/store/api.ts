a35c03cb9c6ca403f39bd6a2554417ccab9707c9
Sasha <q.dev.null@gmail.com>
2022-06-30 09:09:30 +0200

    Fix the naming

--- src/store/api.ts
+++ src/store/api.ts
@@ -2,4 +2,4 @@ import { createAction } from "@reduxjs/toolkit";
 
 export const apiCallBegan = createAction("api/CallBegan");
 export const apiCallSuccess = createAction("api/CallBegan");
-export const apiCallFailed = createAction("api/CallBegan");
+export const apiCallFailed = createAction("api/CallFailed");
