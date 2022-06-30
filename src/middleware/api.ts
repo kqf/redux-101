cdfaaacebdcf2ec8ef6a552dbcf088644a06feca
Sasha <q.dev.null@gmail.com>
2022-06-30 09:06:35 +0200

    Fix the boilerplate code

--- src/middleware/api.ts
+++ src/middleware/api.ts
@@ -1,6 +1,24 @@
 import { Middleware } from "redux"
 import { RootState } from "../store/store"
+import * as actions from "../store/api"
+import axios from "axios"
+
+export const baseUrl: string = "https://api.example.com";
 
 const api: Middleware<{}, RootState> = store => next => async action => {
+    if (action.type !== actions.apiCallBegan) {
+        return next(action)
+    }
+    try {
+        const response = await axios.request({
+            baseUrl: baseUrl,
+            url: action.payload.url,
+            method: action.payload.method,
+            data: action.payload.data,
+        })
+        store.dispatch(actions.apiCallSuccess(response.data));
+    } catch (error) {
+        store.dispatch(actions.apiCallSuccess());
+    }
 
 }
