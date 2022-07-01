7c800a306a0d65741192a05569055ced5a4de34b
kqf <q.dev.null@gmail.com>
2022-07-01 07:48:06 +0200

    Bump api converage (#33)
    * Fix the syntax for calls
    
    * Fix the tests

--- src/store/api.ts
+++ src/store/api.ts
@@ -1,6 +1,12 @@
 import { createAction } from "@reduxjs/toolkit";
 
-export const apiCallBegan = createAction("api/CallBegan");
+export interface APICall {
+    baseURL: string,
+    url: string,
+    method: string,
+    data: any,
+}
+
+export const apiCallBegan = createAction<APICall>("api/CallBegan");
 export const apiCallSuccess = createAction<any>("api/CallBegan");
 export const apiCallFailed = createAction<string>("api/CallFailed");
-
