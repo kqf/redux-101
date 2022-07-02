aaf7b0721b49376b55048d2f99ec05f2776c49fb
Sasha <q.dev.null@gmail.com>
2022-07-02 22:09:26 +0200

    Fix the call middleware

--- src/store/middleware/api.ts
+++ src/store/middleware/api.ts
@@ -2,21 +2,29 @@ import { Middleware } from "redux"
 import { RootState } from "../store"
 import * as actions from "../api"
 import axios from "axios"
+import { PayloadAction } from "@reduxjs/toolkit";
 
 const api: Middleware<{}, RootState> = store => next => async action => {
     if (action.type !== actions.apiCallBegan.type) {
         return next(action)
     }
+
+    const callbeganAction = action as PayloadAction<actions.APICall>;
     try {
         const response = await axios.request({
-            baseURL: action.payload.baseUrl,
-            url: action.payload.url,
-            method: action.payload.method,
-            data: action.payload.data,
+            baseURL: callbeganAction.payload.baseURL,
+            url: callbeganAction.payload.url,
+            method: callbeganAction.payload.method,
+            data: callbeganAction.payload.data,
+        })
+        store.dispatch({
+            "type": callbeganAction.payload.onSuccess,
+            "payload": response.data,
         })
         store.dispatch(actions.apiCallSuccess(response.data));
+
     } catch (error) {
-        store.dispatch(actions.apiCallFailed(error as string));
+        store.dispatch(actions.apiCallFailed("Failed"));
     }
 
 }
