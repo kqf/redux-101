72d5f06119eab54fdda242953539024db71e7be3
kqf <q.dev.null@gmail.com>
2022-07-04 20:04:58 +0200

    Fix middleware (#40)
    * Fix the module structure
    
    * Fix the apis

--- src/store/api.ts
+++ src/store/api.ts
@@ -1,4 +1,7 @@
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, PayloadAction } from "@reduxjs/toolkit";
+import axios from "axios";
+import { Middleware } from "redux";
+import { RootState } from "./store";
 
 export interface APICall {
     baseURL: string,
@@ -11,3 +14,30 @@ export interface APICall {
 export const apiCallBegan = createAction<APICall>("api/CallBegan");
 export const apiCallSuccess = createAction<any>("api/CallBegan");
 export const apiCallFailed = createAction<string>("api/CallFailed");
+
+const api: Middleware<{}, RootState> = store => next => async action => {
+    if (action.type !== apiCallBegan.type) {
+        return next(action)
+    }
+
+    const callbeganAction = action as PayloadAction<APICall>;
+    try {
+        const response = await axios.request({
+            baseURL: callbeganAction.payload.baseURL,
+            url: callbeganAction.payload.url,
+            method: callbeganAction.payload.method,
+            data: callbeganAction.payload.data,
+        })
+        store.dispatch({
+            "type": callbeganAction.payload.onSuccess,
+            "payload": response.data,
+        })
+        store.dispatch(apiCallSuccess(response.data));
+
+    } catch (error) {
+        store.dispatch(apiCallFailed("Failed"));
+    }
+
+}
+
+export default api;
