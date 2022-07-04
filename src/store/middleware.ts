1eb38a8d3d461b61b53dbad15d24f161f48e5fa7
Sasha <q.dev.null@gmail.com>
2022-07-04 20:07:01 +0200

    Fix back apis

--- src/store/middleware.ts
+++ src/store/middleware.ts
@@ -1,4 +1,6 @@
-import { configureStore, Action, Middleware } from '@reduxjs/toolkit';
+import { Middleware, PayloadAction } from '@reduxjs/toolkit';
+import axios from "axios";
+import * as actions from "./api";
 import { RootState } from './store';
 
 export const customLogger: Middleware<{}, RootState> = store => next => action => {
@@ -9,9 +11,36 @@ export const customLogger: Middleware<{}, RootState> = store => next => action =
 };
 
 export const toast: Middleware<{}, RootState> = store => next => action => {
-    if(action.type == "error") {
+    if (action.type == "error") {
         // console.log("error >>> ", action.payload.message)
         return;
     }
     return next(action);
 };
+
+const api: Middleware<{}, RootState> = store => next => async action => {
+    if (action.type !== actions.apiCallBegan.type) {
+        return next(action)
+    }
+
+    const callbeganAction = action as PayloadAction<actions.APICall>;
+    try {
+        const response = await axios.request({
+            baseURL: callbeganAction.payload.baseURL,
+            url: callbeganAction.payload.url,
+            method: callbeganAction.payload.method,
+            data: callbeganAction.payload.data,
+        })
+        store.dispatch({
+            "type": callbeganAction.payload.onSuccess,
+            "payload": response.data,
+        })
+        store.dispatch(actions.apiCallSuccess(response.data));
+
+    } catch (error) {
+        store.dispatch(actions.apiCallFailed("Failed"));
+    }
+
+}
+
+export default api;
