32fa82a946298581814b4e8f9c3e8b44f37c8703
Sasha <q.dev.null@gmail.com>
2022-07-04 20:04:38 +0200

    Fix the apis

--- src/store/api.ts
+++ src/store/api.ts
@@ -1,15 +1,26 @@
-import { Middleware } from "redux"
-import { RootState } from "./store"
-import * as actions from "./api"
-import axios from "axios"
-import { PayloadAction } from "@reduxjs/toolkit";
+import { createAction, PayloadAction } from "@reduxjs/toolkit";
+import axios from "axios";
+import { Middleware } from "redux";
+import { RootState } from "./store";
+
+export interface APICall {
+    baseURL: string,
+    url: string,
+    method: string,
+    data: any,
+    onSuccess: string,
+}
+
+export const apiCallBegan = createAction<APICall>("api/CallBegan");
+export const apiCallSuccess = createAction<any>("api/CallBegan");
+export const apiCallFailed = createAction<string>("api/CallFailed");
 
 const api: Middleware<{}, RootState> = store => next => async action => {
-    if (action.type !== actions.apiCallBegan.type) {
+    if (action.type !== apiCallBegan.type) {
         return next(action)
     }
 
-    const callbeganAction = action as PayloadAction<actions.APICall>;
+    const callbeganAction = action as PayloadAction<APICall>;
     try {
         const response = await axios.request({
             baseURL: callbeganAction.payload.baseURL,
@@ -21,10 +32,10 @@ const api: Middleware<{}, RootState> = store => next => async action => {
             "type": callbeganAction.payload.onSuccess,
             "payload": response.data,
         })
-        store.dispatch(actions.apiCallSuccess(response.data));
+        store.dispatch(apiCallSuccess(response.data));
 
     } catch (error) {
-        store.dispatch(actions.apiCallFailed("Failed"));
+        store.dispatch(apiCallFailed("Failed"));
     }
 
 }
