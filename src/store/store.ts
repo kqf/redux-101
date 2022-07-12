ef7c5c1a5d21d590de1bc613df8f41edd999dd96
kqf <q.dev.null@gmail.com>
2022-07-12 17:36:33 +0100

    Fix the middleware (#42)
    * Fix the module structure
    
    * Fix the apis
    
    * Fix back apis
    
    * Fix the apis

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,9 @@
 import { combineReducers, configureStore, Store } from "@reduxjs/toolkit";
+import api, { customLogger, toast } from "./middleware";
 import orderReducer from "./orders";
-import vendorReducer from "./vendors";
 import ownerReducer from "./owners";
-import { customLogger, toast } from "./middleware";
-import api from "./api";
+import vendorReducer from "./vendors";
+
 
 export const buildStore: () => Store = () => configureStore({
     reducer: combineReducers({
